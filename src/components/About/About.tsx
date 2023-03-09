aa4021441d45109dfb1c34c9d21249a9f48f25b8
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-09 23:36:29 +0200

    working on about seciton

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './About.scss';
 
 export const About = () => {
   return (
-    <div className="App">
-      <h2>About page</h2>
+    <div className="about">
+      <div className="about__page">
+        <h1 className="page__title">About me</h1>
+
+        <p className="about__text">
+        Just call me Nicu! I am a Front End Developer looking to improve myself and my technical skills everyday with passion for technology I combine my programming and design skills with background in game development and 3D Art. My goal is to become a Senior Full Stack Developer and to bring value to the company I’m working for and the people I collaborate with. And of course to prove to myself that my sharp mind and problem solving skills will help me succeed in any environment or problem I am facing. Below are the main technologies I work with!
+
+        </p>
+      </div>
+
+      <div className="about__animation"></div>
     </div>
   );
 }
