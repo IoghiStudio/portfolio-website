b93c55f7af9d59a20df406e347c070d0290ee6f7
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-12 11:32:33 +0200

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -12,15 +12,15 @@ export const Home: React.FC<Props> = ({ onLoad }) => {
     <div className="home">
       <div className="home__page">
         <h1 className="home__title">
-          <span className="home__span">
+          <span className="home__span title-word title-word-1">
             Hi,
           </span>
 
-          <span className="home__span">
+          <span className="home__span title-word title-word-2">
             I'm Nicu,
           </span>
 
-          <span className="home__span">
+          <span className="home__span title-word title-word-3">
             Web developer,
           </span>
         </h1>
