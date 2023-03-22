d885366b8ef7ac64abdb85a3c7d9a0b46956cef0
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-22 18:05:57 +0200

    update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -37,7 +37,7 @@ export const About = () => {
           You can download my CV with pdf format here
           <a
             className='about__cv'
-            href="../../CV/Nicusor Iorga CV.pdf"
+            href="../../CV/nicusoriorgacv.pdf"
             download
           >
             Nicusor Iorga CV.pdf
