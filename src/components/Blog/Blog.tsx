41ea94d2dd397dd6c4b22171557b655fd0712b92
ioghistudio <ioghistudio2038@gmail.com>
2023-07-07 12:00:55 +0300

    test commit

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -5,7 +5,7 @@ export const Blog = () => {
   return (
     <div className="blog">
       <h2 className='blog__title'>My blogs</h2>
-      <p className='blog__text'>Section under development!</p>
+      <p className='blog__text'>!Section under development!</p>
     </div>
   );
 }
